d45cebaad67decc2db887cfb057c176d926897d5
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-19 22:38:59 -0400

    Added some array exercise solutions

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,12 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("The average is: ");
+        int[] numbers = {10, 20, 30,5, 40, 50};
         averageValue();
         findTarget();
         findIndex();
+        removedIndex(numbers, 20);
+        findMaxMin(numbers);
     }
 
     static void averageValue() {
@@ -26,16 +29,18 @@ public class Main {
         boolean isFound = false;
         for (int i = 0; i < numbers.length; i++) {
             if (numbers[i] == target) {
-                System.out.println("The number is found in the array: "+ target+" on index: "+ i);
+                System.out.println("The number is found in the array: " + target + " on index: " + i);
                 isFound = true;
             }
-        }if(isFound != true){
+        }
+        if (isFound != true) {
             System.out.println("It is not found in the array.");
         }
 
     }
-    static void findIndex(){
-        int[] numbers = {10, 20, 50, 40, 50};
+
+    static void findIndex() {
+        int[] numbers = {10, 20, 50, 40,5, 50};
         int target = 40;
         boolean isFound = false;
 
@@ -48,5 +53,50 @@ public class Main {
 
     }
 
+    static int[] removedIndex(int[] numbers, int target) {
+        int[] newArray = new int[numbers.length - 1];
+        int index = 0;
+        for (int i = 0; i < numbers.length; i++) {
+
+            if (numbers[i] != target) {
+                newArray[index] = numbers[i];
+                index++;
+
+            } else {
+
+                System.out.println("Found target " + target + " - skipping it");
+            }
+        }
+
+        return newArray;
+    }
+
+
+    static void findMaxMin(int[]numbers){
+        int min = numbers[0];
+        int max = numbers[0];
+        for(int i = 0; i< numbers.length;i++){
+            System.out.println(numbers[i]);
+            if(min > numbers[i]){
+                min = numbers[i];
+            }
+            if(max< numbers[i]){
+                max = numbers[i];
+            }
+
+        }
+        System.out.println("this is the min number");
+        System.out.println(min);
+        System.out.println("This is the max number");
+        System.out.println(max);
+    }
+
+    static
+
+
+
 
 }
+
+
+
