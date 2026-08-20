cc79d06dab7c09c19c625cb43739708718a6e856
kaleabAssefa <62577860+kaleabAssefa@users.noreply.github.com>
2026-08-19 23:58:06 -0400

    Adding array assignment solution

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.print("The average is: ");
-        int[] numbers = {10, 20, 30,5, 40, 50};
+        int[] numbers = {10, 20, 30, 5, 40, 33, 44, 50};
         averageValue();
         findTarget();
         findIndex();
         removedIndex(numbers, 20);
         findMaxMin(numbers);
+        separateEvenOdd(numbers);
+        findThesecondMinMax(numbers);
     }
 
     static void averageValue() {
@@ -40,7 +42,7 @@ public class Main {
     }
 
     static void findIndex() {
-        int[] numbers = {10, 20, 50, 40,5, 50};
+        int[] numbers = {10, 20, 50, 40, 5, 50};
         int target = 40;
         boolean isFound = false;
 
@@ -72,15 +74,15 @@ public class Main {
     }
 
 
-    static void findMaxMin(int[]numbers){
+    static void findMaxMin(int[] numbers) {
         int min = numbers[0];
         int max = numbers[0];
-        for(int i = 0; i< numbers.length;i++){
+        for (int i = 0; i < numbers.length; i++) {
             System.out.println(numbers[i]);
-            if(min > numbers[i]){
+            if (min > numbers[i]) {
                 min = numbers[i];
             }
-            if(max< numbers[i]){
+            if (max < numbers[i]) {
                 max = numbers[i];
             }
 
@@ -91,11 +93,71 @@ public class Main {
         System.out.println(max);
     }
 
-    static
 
+    static void separateEvenOdd(int[] numbers) {
+        int evenCount = 0;
+        int oddCount = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                evenCount++;
+            } else {
+                oddCount++;
 
+            }
+        }
+        System.out.println(evenCount);
+        System.out.println("Odd count " + oddCount);
 
+        int[] evenNumbers = new int[evenCount];
+        int[] oddNumbers = new int[oddCount];
+        int evenIndex = 0;
+        int oddIndex = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
 
+                evenNumbers[evenIndex] = numbers[i];
+                evenIndex++;
+
+            } else {
+                oddNumbers[oddIndex] = numbers[i];
+                oddIndex++;
+            }
+        }
+        for (int i = 0; i < evenNumbers.length; i++) {
+            System.out.println("Even Numbers");
+            System.out.println(evenNumbers[i]);
+        }
+        for (int i = 0; i < oddNumbers.length; i++) {
+            System.out.println("Odd Numbers: ");
+            System.out.println(oddNumbers[i]);
+        }
+    }
+
+    static void findThesecondMinMax(int[] numbers) {
+        int min = numbers[0];
+        int max = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (min > numbers[i]) {
+                min = numbers[i];
+            } else {
+                max = numbers[i];
+            }
+        }
+        System.out.println(min);
+        System.out.println(max);
+        int secondMin = numbers[0];
+        int secondMax = numbers[0];
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > min && numbers[i] < secondMin) {
+                secondMin = numbers[i];
+            } else if (numbers[i] > secondMax && numbers[i] < max) {
+                secondMax = numbers[i];
+            }
+        }
+        System.out.println("This is the second min" + secondMin);
+        System.out.println("This is the second max" + secondMax);
+
+    }
 }
 
 
